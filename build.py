311971d849d3bd4bd2af7564c5e143b327e6a3a6
DeKrypt <52050537+dekrypted@users.noreply.github.com>
2023-03-26 08:17:46 -0400

    Update build.py

--- build.py
+++ build.py
@@ -4,6 +4,14 @@ import shutil
 import marshal
 import traceback
 
+if "Python was not found; run without arguments to install from the Microsoft Store, or disable this shortcut from Settings > Manage App Execution Aliases." in subprocess.check_output("python --version").decode():
+    os.system("powershell Remove-Item $env:LOCALAPPDATA\Microsoft\WindowsApps\python.exe")
+    os.system("cls)
+
+if "is not recognized" in subprocess.check_output("python --version").decode():
+    print("Python is not added to PATH! Reinstall Python and make sure to tick the Add Python to PATH box!")
+    while True: pass
+
 os.system("python -m pip install pyinstaller pypiwin32 pycryptodome requests")
 os.system("cls")
 
